80773448a58ed26e3791e4437ffb34694d32ae39
Ivo Petrov <1414vo@gmail.com>
2024-07-23 14:55:47 +0100

    exit option in app

--- src/frontend/app.py
+++ src/frontend/app.py
@@ -1,10 +1,17 @@
 import wx
-
+from utils import settings
+from menu import menubar
 class MainFrame(wx.Frame):
     def __init__(self):
         super(MainFrame, self).__init__(None, title = 'Setup', size = (1200, 650))
         
+        self.SetMenuBar(menubar.MenuBar())
+        self.Bind(wx.EVT_MENU, self.OnQuit, id=settings.MenuIDS['FILE_QUIT'])
+        
         self.Centre()
+        
+    def OnQuit(self, e):
+        self.Close()
 
 if __name__ == '__main__':
     app = wx.App()
